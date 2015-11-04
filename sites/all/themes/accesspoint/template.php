a9f9e41aff6e18f263a76e61fab2db50b1a280c8
root <jactadmin@gmail.com>
2015-11-04 15:51:33 -0500

    user agreement style

--- sites/all/themes/accesspoint/template.php
+++ sites/all/themes/accesspoint/template.php
@@ -10,3 +10,11 @@ function accesspoint_preprocess_page(&$variables) {
     $variables['theme_hook_suggestions'][] = 'page__node__' . $variables['node']->type;
   }
 }
+
+function accesspoint_preprocess_flag(&$variables) {
+  if ($variables['flag']->name == "agreement") {
+//     if (arg(0) == 'user') {
+        $variables['theme_hook_suggestions'][] = 'flag__user_agreement';
+    }
+//   }
+}
