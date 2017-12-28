af4bc951efde25ada7da85f25ca8f523a5d5e943
root <root@ip-172-31-11-134.ec2.internal>
2017-12-28 20:14:44 +0000

    Designees directory added

--- sites/all/themes/accesspoint/template.php
+++ sites/all/themes/accesspoint/template.php
@@ -3,7 +3,19 @@
  * @file
  * template.php
  */
+function accesspoint_menu_alter(&$items){
+//dpm($items);
+//  $items['user/%user/user-recurring-entities']['title'] = 'Premier';
 
+}
+
+
+
+function accesspoint_form_alter(&$form, &$form_state, $form_id){
+  if ($form_id == 'user_profile_form') {
+    unset($form['contact']);
+ }
+}
 
 function accesspoint_preprocess_page(&$variables) {
   if (!empty($variables['node']) && !empty($variables['node']->type)) {
